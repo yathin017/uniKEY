89c80219cf136595d274c769437d2565d30755db
Yathin Prakash K <yathinprakash017@gmail.com>
2022-03-13 11:14:43 +0530

    Update Education.sol

--- Education.sol
+++ Education.sol
@@ -3,11 +3,8 @@ pragma solidity ^0.8.4;
 
 contract Education{
     address public Dean;
-    mapping(address=>bool) public collegeAdminID;
-    mapping(address=>bool) public interviewerID;
-    mapping(address=>bool) public studentID;
 
-    struct collegeAdminsDetails{
+    struct collegeAdminDetails{
         string name;
         uint id;
         bool verify;
@@ -30,9 +27,13 @@ contract Education{
     struct courseDetails{
         string courseName;
         uint courseGrade;
+        string courseResult;
     }
 
-    mapping(address=>courseDetails[]) private mapStudentCourse;
+    mapping(address=>collegeAdminDetails) private mapCollegeAdminDetails;
+    mapping(address=>interviewerDetails) private mapInterviewerDetails;
+    mapping(address=>studentDetails) private mapStudentDetails;
+    mapping(address=>mapping(uint=>courseDetails[])) private mapStudentCourse;
 
     modifier onlyDean(){
         require(msg.sender==Dean,"Only owner can calll this function");
@@ -40,12 +41,12 @@ contract Education{
     }
 
     modifier onlyCollegeAdmins(){
-        require(collegeAdminID[msg.sender]==true,"Only owner can calll this function");
+        require(mapCollegeAdminDetails[msg.sender].verify==true,"Only owner can calll this function");
         _;
     }
 
     modifier restrictedAccess(){
-        require(msg.sender==Dean || collegeAdminID[msg.sender]==true || interviewerID[msg.sender]==true || studentID[msg.sender]==true,"Only Dean, College Admins and Students can calll this function");
+        require(msg.sender==Dean || mapCollegeAdminDetails[msg.sender].verify==true || mapCollegeAdminDetails[msg.sender].verify==true || mapStudentDetails[msg.sender].verify==true,"Only Dean, College Admins and Students can calll this function");
         _;
     }
 
@@ -64,57 +65,69 @@ contract Education{
     // College Admin Details
     // Set
     function setCollegeAdmin(address _collegeAdmin, string memory _name, uint _id) public onlyCollegeAdmins onlyDean{
-
+        mapCollegeAdminDetails[_collegeAdmin] = collegeAdminDetails(_name, _id, true);
     }
     // Get
-    function getCollegeAdmin(address _collegeAdmin) public returns(string memory, uint){
-        
+    function getCollegeAdmin(address _collegeAdmin) public view returns(string memory, uint, bool){
+        return(mapCollegeAdminDetails[_collegeAdmin].name, mapCollegeAdminDetails[_collegeAdmin].id, mapCollegeAdminDetails[_collegeAdmin].verify);
     }
     // Remove
     function removeCollegeAdmin(address _collegeAdmin) public onlyDean{
-
+        mapCollegeAdminDetails[_collegeAdmin].verify = false;
     }
 
     // Interviewer Details
     // Set
     function setInterviewer(address _interviewer, string memory _name, uint _id, string memory _companyName) public onlyCollegeAdmins onlyDean{
-
+        mapInterviewerDetails[_interviewer] = interviewerDetails(_name, _id, _companyName, true);
     }
     // Get
-    function getInterviewer(address _interviewer) public returns(string memory, uint, string memory){
-        
+    function getInterviewer(address _interviewer) public view returns(string memory, uint, string memory, bool){
+        return(mapInterviewerDetails[_interviewer].name, mapInterviewerDetails[_interviewer].id, mapInterviewerDetails[_interviewer].companyName, mapInterviewerDetails[_interviewer].verify);
     }
     // Remove
     function removeInterviewer(address _interviewer) public onlyDean{
-        
+        mapInterviewerDetails[_interviewer].verify = false;
     }
 
     // Student Details
     // Set
     function setStudent(address _student, string memory _name, uint _id, string memory _dept) public onlyCollegeAdmins onlyDean{
-
+        mapStudentDetails[_student] = studentDetails(_name, _id, _dept, true);
     }
     // Get
-    function getStudent(address _student) public returns(string memory, uint, string memory){
-
+    function getStudent(address _student) public view returns(string memory, uint, string memory, bool){
+        return (mapStudentDetails[_student].name, mapStudentDetails[_student].id, mapStudentDetails[_student].dept, mapStudentDetails[_student].verify);
     }
     // Remove
-    function removeStudent(address _interviewer) public onlyDean{
-        
+    function removeStudent(address _student) public onlyDean{
+        mapStudentDetails[_student].verify = false;
     }
 
     // Course and Grades
     // Set
-    function addCompletedCourses(address _student) public onlyCollegeAdmins{
-
+    function addCompletedCourses(address _student, uint _sem, string memory _courseName, uint _courseGrade, string memory _courseResult) public onlyCollegeAdmins{
+        mapStudentCourse[_student][_sem].push(courseDetails({
+            courseName:_courseName,
+            courseGrade:_courseGrade,
+            courseResult:_courseResult
+        }));
     }
     // Get
     function viewCompletedCourses(address _student) public view restrictedAccess returns(courseDetails[] memory){
-
+        uint _semNo;
+        courseDetails[] memory sem = new courseDetails[](mapStudentCourse[_student][_semNo].length);
+        
+        for(_semNo=1; _semNo<mapStudentCourse[_student][_semNo].length; _semNo++){
+            for(uint index=0; index<mapStudentCourse[_student][_semNo].length; index++){
+                sem[index] = mapStudentCourse[_student][_semNo][index];
+            }
+        }
+        return sem;
     }
 
     // Degree
     function viewDegree(address _student) public view returns(address, string memory, uint, string memory){
-        
+        return(_student, mapStudentDetails[_student].name, mapStudentDetails[_student].id, mapStudentDetails[_student].dept);
     }
 }
