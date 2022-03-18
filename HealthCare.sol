83a6942073bce950b73bb370d9f7ac9f531c88c9
Yathin Prakash K <yathinprakash017@gmail.com>
2022-03-18 10:56:35 +0530

    Update HealthCare.sol

--- HealthCare.sol
+++ HealthCare.sol
@@ -29,15 +29,20 @@ contract HealthCare{
         string diagnosys;
     }
 
+    struct Viewer{
+        mapping(address=>bool) viewerID;
+    }
+
     mapping(address=>patientInfo) public viewPatientInfo;
     mapping(address=>hospitalInfo) public viewHospitalInfo;
     mapping(address=>patientRecord[]) private viewPatientRecord;
+    mapping(address=>Viewer) private viewers;
 
     constructor(){
         contractAdmin = msg.sender;
     }
 
-    function setPatientInfo(address _patientID, string memory _patientName, string memory _DOB, string memory _bloodGroup, string memory _gender) public {
+    function setPatientInfo(address _patientID, string memory _patientName, string memory _DOB, string memory _bloodGroup, string memory _gender) public{
         require(patientID[_patientID]==false,"You are already an existing user");
         require(_patientID==msg.sender,"Use your account to add details");
         require(viewPatientRecord[_patientID].length==0,"This account is removed from services... Please try with different account");
@@ -51,7 +56,7 @@ contract HealthCare{
         hospitalID[_hospitalID] = true;
     }
 
-    function setPatientRecord(address _patientID, string memory _hospitalName, string memory _doctorName, string memory _healthIssue, string memory _medicines, string memory _reportAnalysis, string memory _diagnosys) public {
+    function setPatientRecord(address _patientID, string memory _hospitalName, string memory _doctorName, string memory _healthIssue, string memory _medicines, string memory _reportAnalysis, string memory _diagnosys) public{
         require(patientID[_patientID]==true,"Set Patient Information First");
         require(hospitalID[msg.sender]==true,"You cannot set Patient's Record");
         viewPatientRecord[_patientID].push(patientRecord({
@@ -63,34 +68,42 @@ contract HealthCare{
             reportAnalysis:_reportAnalysis,
             diagnosys:_diagnosys
         }));
+        viewers[_patientID].viewerID[msg.sender]=true;
     }
 
     function recentPatientRecord(address _patientID) public view returns(patientRecord memory){
         require(patientID[_patientID]==true,"Set Patient Information First");
         require(viewPatientRecord[_patientID].length>0,"No Patient Record");
+        require(_patientID==msg.sender || viewers[_patientID].viewerID[msg.sender]==true,"You cannot view this information");
         uint index = viewPatientRecord[_patientID].length-1;
         return (viewPatientRecord[_patientID][index]);
-    }
-
-    function removeHospital(address _hospitalID) public {
-        require(contractAdmin==msg.sender);
-        require(hospitalID[_hospitalID]==true,"Hospital does not exist");
-        hospitalID[_hospitalID] = false;
-    }
-
-    function removePatient(address _patientID) public {
-        require(contractAdmin==msg.sender || _patientID==msg.sender);
-        patientID[_patientID] = false;
     }
 
     function AllPatientRecord(address _patientID) public view returns(patientRecord[] memory){
         require(patientID[_patientID]==true,"Set Patient Information First");
         require(viewPatientRecord[_patientID].length>0,"No Patient Record");
+        require(_patientID==msg.sender || viewers[_patientID].viewerID[msg.sender]==true,"You cannot view this information");
         patientRecord[] memory data = new patientRecord[](viewPatientRecord[_patientID].length);
         for(uint index=0; index<viewPatientRecord[_patientID].length; index++){
             data[index] = viewPatientRecord[_patientID][index];
         }
         return (data);
+    }
+
+    function removeHospital(address _hospitalID) public{
+        require(contractAdmin==msg.sender);
+        require(hospitalID[_hospitalID]==true,"Hospital does not exist");
+        hospitalID[_hospitalID] = false;
+    }
+
+    function removePatient(address _patientID) public{
+        require(contractAdmin==msg.sender || _patientID==msg.sender);
+        patientID[_patientID] = false;
+    }
+
+    function removeViewer(address _viewerID) public{
+        require(patientID[msg.sender]==true,"Set Patient Information First");
+        viewers[msg.sender].viewerID[_viewerID]=false;
     }
     
 }
